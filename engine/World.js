2277e795dd4b36cc0a616f690ddcd0efc3fa091c
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-05-02 10:21:44 -0400

    Update World.js

--- engine/World.js
+++ engine/World.js
@@ -1,7 +1,18 @@
 class World{
-	constructor(){}
+	constructor(){
+		try{
+		this.canvas = document.getElementById("board");
+		this.ctx = this.canvas.getContext("2d");
+		}catch(e){
+			alert(e);
+		}
+	}
 	update(){}
-	draw(){}
+	draw(){
+		this.ctx.font = "25px serif";
+		this.ctx.fillStyle = "#000000";
+		this.ctx.fillText(":/", 15, 30);
+	}
 	mousePress(){}
 	keyPress(){}
 	
