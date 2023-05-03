235ba83664d2846dac00b109441925a3ad226dca
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-05-03 11:05:14 -0400

    Update World.js

--- engine/World.js
+++ engine/World.js
@@ -1,34 +1,42 @@
 import { v2 } from "./v2.js";
-export class World{
-	constructor(){
-		this.canvas = document.getElementById("board");
-		this.ctx = this.canvas.getContext("2d");
-		this.map = {};
-		this.player = {pos:new v2(0,0)};
-		this.grid = true;
-		this.gridSize = 64;
-	}
-	setPlayer(p){
-		this.player = p;
-	}
-	update(){}
-	draw(){
-		this.ctx.font = "25px serif";
-		this.ctx.fillStyle = "#000000";
-		this.ctx.fillText(":/", 15, 30);
-		if(this.grid){
-			for(var x = this.player.pos.x % this.gridSize-63; x < this.canvas.width; x+=this.gridSize){
-				for(var y = this.player.pos.y % this.gridSize-63; y < this.canvas.height; y+=this.gridSize){
-					this.ctx.strokeStyle = "rgb(0,0,0)";
-					this.ctx.strokeRect(x,y,this.gridSize,this.gridSize);
-				}
-			}
-		}
-	}
-	mousePress(pos, btn){}
-	keyPress(key, state){}
-	
-	addEnt(e, pos){}
-	moveEnt(e, pos){}
-	removeEnt(e){}
+export class World {
+  constructor() {
+    this.canvas = document.getElementById("board");
+    this.ctx = this.canvas.getContext("2d");
+    this.map = {};
+    this.player = { pos: new v2(0, 0) };
+    this.grid = true;
+    this.gridSize = 64;
+  }
+  setPlayer(p) {
+    this.player = p;
+  }
+  update() {}
+  draw() {
+    this.ctx.font = "25px serif";
+    this.ctx.fillStyle = "#000000";
+    this.ctx.fillText(":/", 15, 30);
+    if (this.grid) {
+      for (
+        var x = (this.player.pos.x % this.gridSize) - 63;
+        x < this.canvas.width;
+        x += this.gridSize
+      ) {
+        for (
+          var y = (this.player.pos.y % this.gridSize) - 63;
+          y < this.canvas.height;
+          y += this.gridSize
+        ) {
+          this.ctx.strokeStyle = "rgb(0,0,0)";
+          this.ctx.strokeRect(x, y, this.gridSize, this.gridSize);
+        }
+      }
+    }
+  }
+  mousePress(pos, btn) {}
+  keyPress(key, state) {}
+
+  addEnt(e, pos) {}
+  moveEnt(e, pos) {}
+  removeEnt(e) {}
 }
