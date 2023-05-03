578573e8c0c23c976ce0f86418ff56430c646f2c
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-05-03 13:34:11 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 import { World } from "./World.js";
 alert("a");
 try{
+	var draw = new DrawParams();
+	draw.color = "#808080";
+	var ent = new EntityBase(new v2(4,5), new v2(1,1), draw);
 	var a = new World();
+	a.addEnt(ent)
 	a.draw();
 }catch(e){
 	alert(e);
