c25100fa58b62147316281385b797d7cc610fe77
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-05-03 18:36:21 -0400

    Update World.js

--- World.js
+++ World.js
@@ -40,8 +40,10 @@ export class World {
 			for(const [x,v] of Object.entries(this.map)){
 				for(const [y,o] of Object.entries(v)){
 					alert(typeof(x));
+					var x2 = parseInt(x);
+					var y2 = parseInt(y);
 					this.ctx.fillStyle = o.draw.color;
-					this.ctx.fillRect((x + this.player.pos.x)* this.gridSize, (y + this.player.pos.y) * this.gridSize, this.gridSize, this.gridSize);
+					this.ctx.fillRect((x2 + this.player.pos.x)* this.gridSize, (y2 + this.player.pos.y) * this.gridSize, this.gridSize, this.gridSize);
 				}
 			}
 		}
