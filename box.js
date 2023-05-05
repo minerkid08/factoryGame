a8007ecbdf0cfb9a88d292dd39d19917d4ac4983
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-05-05 13:47:19 -0400

    Create box.js

---
+++ box.js
@@ -0,0 +1,27 @@
+import { v2 } from "./v2.js";
+import { EntityBase } from "./entityBase.js";
+export class Box{
+	start(){
+		this.draw.color = "#800000";
+		this.maxDist = 3;
+		this.dir = true;
+		this.step = 0;
+	}
+	update(){
+		if(this.dir){
+			this.pos.x += 1;
+			this.step += 1;
+			if(this.step == maxDist){
+				this.dir = false;
+				return;
+			}
+		}else{
+			this.pos.x -= 1;
+			this.step -= 1;
+			if(this.step == 0){
+				this.dir = true;
+				return;
+			}
+		}
+	}
+}
